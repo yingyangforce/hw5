54a49e1fcb9cf64677c40317500f9b699c059e51
Gene Calmer <yingyangforce@gmail.com>
2022-03-11 16:24:26 -0500

    make medium rare

---
+++ grace_hw5a.py
@@ -0,0 +1,21 @@
+# File: grace-hw5a.py
+# Author: Isaiah Grace
+# Date: 2022/3/11
+# Lab Section: Tuesday
+# Email: isaiah.grace@maine.edu
+# Description: checks if second char == second to last char
+# Collabs: N/a
+
+def nameChecker(name) -> bool:
+    if len(name) <= 1:
+        return False
+    return True if name[1] == name[-2] else False
+
+usr_name = input("Enter a name: ")
+
+while(nameChecker(usr_name)):
+    print("The second and penultimate chars are the same!")
+    usr_name = input("Enter another name? Else enter n: ")
+
+print("Goodbye")
+
